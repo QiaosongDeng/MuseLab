97587cc7b3fd51d124f66f93c4645e4a80e7cf4a
QiaosongDeng <qiaosong@ualberta.ca>
2023-10-18 01:52:54 -0400

    Update Android version and Add Navigation View to switch between Muse and EOG activities

---
+++ app/src/main/java/com/example/muselab/EOGActivity.java
@@ -0,0 +1,63 @@
+package com.example.muselab;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.content.Intent;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class EOGActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eogactivity);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_muse){
+            Toast.makeText(EOGActivity.this,"Muse",Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(EOGActivity.this, MainActivity.class));
+        } else if (id == R.id.nav_eog) {
+            Toast.makeText(EOGActivity.this,"EOG",Toast.LENGTH_SHORT).show();
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return false;
+    }
+}
